0bd1ff07c10462d535c54c8d4c93907bf54e1d59
edom18 <edo.m18@gmail.com>
2013-11-12 16:57:52 +0900

    added

---
+++ near-line/js/base.js
@@ -0,0 +1,28 @@
+(function (exports) {
+    'use strict';
+
+    var Object2D = Class.extend({
+        init: function () {
+
+        },
+        draw: function (ctx) {
+
+        },
+        update: function () {
+
+        }
+    });
+
+    var Line = Object2D.extend();
+
+    var Point = Object2D.extend();
+
+    var Triangle = Object2D.extend();
+
+
+    /*! ---------------------------------------------------------
+        EXPORTS
+    ------------------------------------------------------------- */
+    exports.Object2D = Object2D;
+
+}(window));
