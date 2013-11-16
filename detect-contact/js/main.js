015350f28daae64557632e7fd95f226001e2d3b1
edom18 <edo.m18@gmail.com>
2013-11-16 21:43:46 +0900

    update.

--- detect-contact/js/main.js
+++ detect-contact/js/main.js
@@ -39,7 +39,7 @@
             color: 'red',
             mass: 5
         });
-        //triangle1.translate(vec2(100, 100));
+        triangle1.translate(vec2(-350, -350));
         scene.add(triangle1);
 
         var triangle2 = new Phys2D.Triangle(v4, v5, v6, {
@@ -71,6 +71,7 @@
             init: function (objA, objB) {
                 this._objA = objA;
                 this._objB = objB;
+                this._foundPoints = [];
 
                 this._detect();
             },
@@ -89,9 +90,111 @@
 
                 return vec2(ful);
             },
+
+            _isContact: function () {
+
+                var v0 = this._foundPoints[0];
+                var v1 = this._foundPoints[1];
+                var v2 = this._foundPoints[2];
+
+                var edge0 = vec2.sub(v1, v0);
+                var edge1 = vec2.sub(v2, v1);
+                var edge2 = vec2.sub(v0, v2);
+
+                var ce0 = vec2.sub(v1, v0);
+                var ce1 = vec2.sub(v2, v0);
+                var CCW = 1;
+
+                if (vec2.cross(ce0, ce1) < 0) {
+                    CCW = -1;
+                }
+
+                var cp0 = vec2.sub(originPos, v0);
+                
+                if (vec2.cross(edge0, cp0) * CCW < 0) {
+                    return false;
+                }
+
+                var cp1 = vec2.sub(originPos, v1);
+                
+                if (vec2.cross(edge1, cp1) * CCW < 0) {
+                    return false;
+                }
+
+                var cp2 = vec2.sub(originPos, v2);
+                
+                if (vec2.cross(edge2, cp2) * CCW < 0) {
+                    return false;
+                }
+
+                return true;
+            },
+
+            _checkCenterInTriangle: function () {
+
+                var objAVert = this._objA.getVertices();
+                var objBVert = this._objB.getVertices();
+                var points   = this._foundPoints;
+                var len = points.length;
+                var supportA, supportB,
+                    ful, fulA, fulB;
+
+                //三角形内に原点があった場合は衝突している
+                if (this._isContact()) {
+                    return true;
+                }
+
+                //ひとつ前の点から最新の点への線分への最近接点を求める
+                var nearPoint = Phys2D.detectPointOnLine(this._foundPoints[0], this._foundPoints[1], originPos);
+
+                //求まった点と原点を結んだベクトルのサポート写像を求める
+                supportA = vec2.sub(originPos, nearPoint);
+                vec2.normalize(supportA);
+                supportB = vec2.minus(supportA);
+
+                fulA = this._support(supportA, objAVert);
+                fulB = this._support(supportB, objBVert);
+
+                //支点
+                ful = vec2.sub(fulA, fulB);
+
+                //三角形の中に原点が見つからず、
+                //かつ検出できる支点がなくなった場合は衝突していない
+                if (vec2.equal(ful, this._foundPoints[0])) {
+                    return false;
+                }
+
+                this._foundPoints.unshift(ful);
+
+                //見つかった点を3点までに制限する
+                this._slicePoints();
+
+                return this._checkCenterInTriangle();
+            },
+
+            /**
+             * 求まった点の中で、原点から一番遠い点を除外
+             */
+            _slicePoints: function () {
+
+                var points = this._foundPoints;
+                var distance = 0,
+                    point;
+
+                for (var i = 0, l = points.length; i < l; i++) {
+                    var len = vec2.norm(points[i]);
+                    if (distance < len) {
+                        distance = len;
+                        point = points[i];
+                    }
+                }
+
+                var index = points.indexOf(point);
+                points.splice(index, 1);
+            },
+
             _detect: function () {
 
-                var fuls = [];
                 var objAVert = this._objA.getVertices();
                 var objBVert = this._objB.getVertices();
                 var supportA, supportB,
@@ -101,10 +204,10 @@
                 supportA = vec2.sub(originPos, this._objA.getCenter());
 
                 //中心から原点へのベクトルを視覚化
-                var line = new Phys2D.Line(originPos, this._objA.getCenter(), {
-                    color: '#fff'
-                });
-                scene.add(line);
+                // var line = new Phys2D.Line(originPos, this._objA.getCenter(), {
+                //     color: '#fff'
+                // });
+                // scene.add(line);
 
                 //ベクトルを正規化
                 supportA = vec2.normalize(supportA);
@@ -115,65 +218,72 @@
                 //objAの支点を求める
                 fulA = this._support(supportA, objAVert);
 
-                var pointA = new Phys2D.Point(vec2(fulA), {
-                    color: 'green'
-                });
-                scene.add(pointA);
+                // var pointA = new Phys2D.Point(vec2(fulA), {
+                //     color: 'green'
+                // });
+                // scene.add(pointA);
 
                 //objBの支点を求める
                 fulB = this._support(supportB, objBVert);
 
-                var pointB = new Phys2D.Point(vec2(fulB), {
-                    color: 'green'
-                });
-                scene.add(pointB);
+                // var pointB = new Phys2D.Point(vec2(fulB), {
+                //     color: 'green'
+                // });
+                // scene.add(pointB);
 
+                //支点1
                 //求めたA,Bのサポート写像を合成し、ミンコフスキ差としての支点を求める
                 var ful = vec2.sub(fulA, fulB);
-                fuls.push(ful);
+                this._foundPoints.unshift(ful);
 
-                var pointC = new Phys2D.Point(vec2(ful), {
-                   color: 'green'
-                });
-                scene.add(pointC);
+                // var pointC = new Phys2D.Point(vec2(ful), {
+                //    color: 'green'
+                // });
+                // scene.add(pointC);
 
-                // //求まった点と原点を結んだベクトルのサポート写像を求める
-                // supportA = vec2.sub(originPos, ful);
-                // vec2.normalize(supportA);
-                // supportB = vec2.minus(supportA);
+                //求まった点と原点を結んだベクトルのサポート写像を求める
+                supportA = vec2.sub(originPos, ful);
+                supportA = vec2.normalize(supportA);
+                supportB = vec2.minus(supportA);
 
-                // fulA = this._support(supportA, objAVert);
-                // fulB = this._support(supportB, objBVert);
-                // ful = vec2.sub(fulA, fulB);
-                // fuls.push(ful);
+                fulA = this._support(supportA, objAVert);
+                fulB = this._support(supportB, objBVert);
+
+                //支点2
+                ful = vec2.sub(fulA, fulB);
+                this._foundPoints.unshift(ful);
 
                 // var pointD = new Phys2D.Point(vec2(ful), {
                 //    color: 'green'
                 // });
                 // scene.add(pointD);
 
-                // var nearPoint = Phys2D.detectPointOnLine(fuls[1], fuls[0], originPos);
+                //求まった2つの点で作られる線分の最接近点を求める
+                var nearPoint = Phys2D.detectPointOnLine(this._foundPoints[1], this._foundPoints[0], originPos);
+
                 // var pointE = new Phys2D.Point(vec2(nearPoint), {
                 //     color: 'green'
                 // });
                 // scene.add(pointE);
 
-                // //求まった点と原点を結んだベクトルのサポート写像を求める
-                // supportA = vec2.sub(originPos, nearPoint);
-                // vec2.normalize(supportA);
-                // supportB = vec2.minus(supportA);
+                //求まった点と原点を結んだベクトルのサポート写像を求める
+                supportA = vec2.sub(originPos, nearPoint);
+                vec2.normalize(supportA);
+                supportB = vec2.minus(supportA);
 
-                // fulA = this._support(supportA, objAVert);
-                // fulB = this._support(supportB, objBVert);
-                // ful = vec2.sub(fulA, fulB);
-                // fuls.push(ful);
+                fulA = this._support(supportA, objAVert);
+                fulB = this._support(supportB, objBVert);
 
-                // //3点求まったので三角形を作る
-                // var tri = new Phys2D.Triangle(fuls[0], fuls[1], fuls[2], {
-                //     color: 'green'
-                // });
+                //支点3
+                ful = vec2.sub(fulA, fulB);
+                this._foundPoints.unshift(ful);
 
-                // scene.add(tri);
+                if (this._checkCenterInTriangle()) {
+                    triangle1.setColor('gray');
+                }
+                else {
+                    triangle1.setColor('#c00');
+                }
             }
         });
 
@@ -184,71 +294,33 @@
                 var contact = new Contact(triangle1, triangle2);
             });
 
-            var lines = [], points = [];
-            document.addEventListener('mouseup', function (e) {
+            var prevX = 0;
+            var prevY = 0;
+            var dragging = false;
 
+            cv.addEventListener('mousedown', function (e) {
+                prevX = e.pageX;
+                prevY = e.pageY;
+                dragging = true;
+            }, false)
+
+            document.addEventListener('mouseup', function (e) {
+                dragging = false;
+            }, false)
+
+            document.addEventListener('mousemove', function (e) {
                 if (!dragging) {
                     return;
                 }
+                prevX = e.pageX - prevX;
+                prevY = e.pageY - prevY;
 
-                dragging = false;
-                endPos = Phys2D.convertPoint(e.pageX, e.pageY);
+                triangle1.translate(vec2(prevX, -prevY));
+                var contact = new Contact(triangle1, triangle2);
 
-                var color = '#666';
-                lines.forEach(function (line) {
-                    line.setColor(color);
-                });
-                points.forEach(function (point) {
-                    point.setColor(color);
-                });
-
-                //引かれたラインと原点との最近接点を検出
-                var detectVec = Phys2D.detectPointOnLine(startPos, endPos, originPos);
-
-                //検出された点を作成
-                var point = new Phys2D.Point(detectVec, {
-                    radius: 3,
-                    color: '#fff'
-                });
-                scene.add(point);
-                points.push(point);
-
-                //引かれたラインを残す用
-                var line1 = new Phys2D.Line(startPos, endPos, {
-                    color: '#fff'
-                });
-                scene.add(line1);
-                lines.push(line1);
-
-                //検出した点を通るライン
-                var dx = vec2.sub(detectVec, originPos);
-                var delta = dx.y / dx.x;
-                var ey = -hw * delta;
-                var e0 = vec2(-hw, ey);
-                var e1 = vec2(hw, -ey)
-
-                var line2 = new Phys2D.Line(e0, e1, {
-                    color: '#1191fa'
-                });
-                scene.add(line2);
-                lines.push(line2);
-
-                //内積を取るようのベクトルを算出
-                var _detectVec = vec2(detectVec);
-                _detectVec = vec2.normalize(_detectVec);
-
-                var vertices = triangle1.getVertices();
-                for (var i = 0, l = vertices.length; i < l; i++) {
-                    var dot = vec2.dot(_detectVec, vertices[i]);
-                    var vec = vec2.multiplyScalar(_detectVec, dot);
-                    var dp = new Phys2D.Point(vec, {
-                        color: 'green'
-                    });
-                    scene.add(dp);
-                    points.push(dp);
-                }
-
-            }, false);
+                prevX = e.pageX;
+                prevY = e.pageY;
+            }, false)
         }
     }, false);
 }());
