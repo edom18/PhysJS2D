03f53db6e7777e84a8ff50c50abd81768ffa9562
edom18 <edo.m18@gmail.com>
2013-12-10 17:40:08 +0900

    implemented a box shape.

--- world/js/main.js
+++ world/js/main.js
@@ -34,50 +34,16 @@
         var v7 = vec2(-1.5, 1.55);
         var v8 = vec2(  0.0, 2.155);
         var v9 = vec2( 0.65, 0.45);
-
-        var triangle1 = new Phys2D.Triangle(v1, v2, v3, {
-            angularVelocity: -1,
-            acceleration: vec2(0, -9.8),
-            // velocity: vec2(0, -10),
-            color: 'red',
-            mass: 100
-        });
-        triangle1.translate(vec2(0, 10));
-        // triangle1.scale(vec2(0.5));
-        // triangle1.rotate(-1);
-        // window.t = triangle1;
-        // scene.add(triangle1);
-
-        var triangle2 = new Phys2D.Triangle(v4, v5, v6, {
-            // angularVelocity: 0.1,
-            color: 'blue',
-            mass: 0
-        });
-        triangle2.translate(vec2(0, -70));
-        // triangle2.scale(vec2(0.32));
-        // scene.add(triangle2);
-
-        var triangle3 = new Phys2D.Triangle(v7, v8, v9, {
-            angularVelocity: 50,
-            acceleration: vec2(0, -9.8),
-            color: 'green',
-            mass: 20
-        });
-        triangle3.scale(vec2(0.7));
-        triangle3.translate(vec2(0, 20));
-        // scene.add(triangle3);
-
-        var triangle4 = new Phys2D.Triangle(v7, v8, v9, {
-            angularVelocity: 30,
-            acceleration: vec2(0, -9.8),
-            color: 'yellow',
-            mass: 50
-        });
-        // triangle4.translate(vec2(-100, 0));
-        // triangle4.rotate(55);
-        triangle4.scale(vec2(0.1));
-        // scene.add(triangle4);
         
+        var redShape = new Phys2D.BoxShape(2, 2);
+        var redBody = new Phys2D.RigidBody(20, redShape, {
+            // angularVelocity: 500,
+            acceleration: vec2(0, -9.8),
+            color: 'red'
+        });
+        redBody.translate(vec2(0.5, 5));
+        scene.add(redBody);
+
         var blueShape = new Phys2D.TriangleShape(v4, v5, v6);
         var blueBody = new Phys2D.RigidBody(0, blueShape, {
             color: 'blue'
@@ -86,21 +52,27 @@
         blueBody.translate(vec2(0, -10));
         scene.add(blueBody);
 
-        var greenShape = new Phys2D.TriangleShape(v7, v8, v9);
-        var greenBody = new Phys2D.RigidBody(20, greenShape, {
-            // angularVelocity: 50,
+        var greenShape = new Phys2D.BoxShape(1, 1);
+        var greenBody = new Phys2D.RigidBody(200, greenShape, {
+            // angularVelocity: 500,
             acceleration: vec2(0, -9.8),
             color: 'green'
         });
         scene.add(greenBody);
 
+        var yellowShape = new Phys2D.TriangleShape(v1, v2, v3);
+        var yellowBody = new Phys2D.RigidBody(10, yellowShape, {
+            acceleration: vec2(0, -9.8),
+            color: 'yellow'
+        });
+        yellowBody.translate(vec2(0, 1));
+        scene.add(yellowBody);
+
         var world = new Phys2D.World();
+        world.add(redBody);
         world.add(blueBody);
         world.add(greenBody);
-        // world.add(triangle1);
-        // world.add(triangle2);
-        // world.add(triangle3);
-        // world.add(triangle4);
+        world.add(yellowBody);
 
         //直交座標系のラインを引く
         var baseLine1 = new Phys2D.Line(vec2(-hw, 0), vec2(hw, 0), {
